503f21ea4071fd4101c1cc69285d2a4650a584f8
leonardosoares1 <dev@doleonardo.com.br>
2022-10-12 21:24:31 -0300

    feat: exportando formatadores

---
+++ src/formatters/index.ts
@@ -0,0 +1,17 @@
+import cnpj from './cnpj';
+import cpf from './cpf';
+import money from './money';
+import phone from './phone';
+import postalCode from './postalCode';
+import removeMask from './removeMask';
+
+const formatters = {
+  cnpj,
+  cpf,
+  money,
+  phone,
+  postalCode,
+  removeMask,
+};
+
+export default formatters;
