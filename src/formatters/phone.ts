39643fc283685ef969f56d70e368545e55ecc5fa
leonardosoares1 <dev@doleonardo.com.br>
2022-10-12 21:23:03 -0300

    feat: adicionando formatador de telefone

---
+++ src/formatters/phone.ts
@@ -0,0 +1,26 @@
+interface IPhoneOptions {
+  defaultValue?: string;
+}
+
+function phone(value?: string | null, options?: IPhoneOptions): string {
+  const defaultValue = options?.defaultValue || '';
+  if (!value) {
+    return defaultValue;
+  }
+  const valueClean = value.replace(/[^0-9]/g, '');
+  if (valueClean.substring(0, 4) === '0800') {
+    const specialPhoneFormatted = value
+      .replace(/[^0-9]/g, '')
+      .replace(/(\d{4})(\d)/, '$1 $2')
+      .replace(/(\d{4}) (\w+)/, '$1 $2')
+      .replace(/(\d{4}) (\w+)(\d{4})/, '$1 $2 $3');
+    return specialPhoneFormatted;
+  }
+  const commonPhoneFormatted = valueClean
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4,5})(\d{4})/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1');
+  return commonPhoneFormatted;
+}
+
+export default phone;
